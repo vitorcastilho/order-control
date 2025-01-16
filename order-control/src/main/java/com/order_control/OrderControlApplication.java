d153b6a22189a710fc1329043ccc599da11f606c
vitorcastilho <vitorcastilho@msn.com>
2025-01-16 17:46:27 -0300

    backend creation

---
+++ order-control/src/main/java/com/order_control/OrderControlApplication.java
@@ -0,0 +1,13 @@
+package com.order_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderControlApplication.class, args);
+	}
+
+}
